d1013ef329c555b48733bd6bef88bdeb1ff3712d
sult4nzoy <OmarKabir91@hotmail.com>
2025-10-21 20:03:25 +0200

    ännu mer kod

--- Pmetodik/Labb5CLION/main.cpp
+++ Pmetodik/Labb5CLION/main.cpp
@@ -10,8 +10,8 @@ int main() {
     Car carArray[arrsize] = {
 
         Car("Audi", 1543),
-        Car("BMW", 1367),
-        Car("Audi", 1465),
+        Car("Audi", 1367),
+        Car("BMW", 1465),
         Car("Subaru", 1187),
         Car("Honda", 1987),
         Car("Volvo", 2500),
@@ -49,6 +49,7 @@ int main() {
     std::cout << "-------------------------------------------------------" << std::endl;
     std::cout << std::endl;
     std::cout << "3. Gå igenom vector med adjacent_find och hitta alla bilar med samma namn." << std::endl;
+
     auto SameName = std::adjacent_find(cars.begin(), cars.end());
     if (SameName != cars.end()) {
         printer(*SameName);
@@ -57,6 +58,33 @@ int main() {
         std::cout << "Hitta ej" << std::endl;
     }
     std::cout << "-------------------------------------------------------" << std::endl;
+    std::cout << std::endl;
+    std::cout << "4. Kolla om arrayen och vectorn har samma innehåll, det görs med std::equal" << std::endl;
+
+    auto sameContainers = std::equal(cars.begin(), cars.end(), carArray);
+
+    if (sameContainers == true) {
+        std::cout << "Arr och vec har samma innehåll" << std::endl;
+    }
+    else {
+        std::cout << "Ej samma innehåll" << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "-------------------------------------------------------" << std::endl;
+    std::cout << "5. Kolla om arrayen och vectorn har samma innehåll, det görs med std::equal" << std::endl;
+    std::vector<Car> subSequence;
+    subSequence.push_back(cars[4]);
+    subSequence.push_back(cars[5]);
+    subSequence.push_back(cars[6]);
+    auto subSeqFound = std::search(cars.begin(), cars.end(), subSequence.begin(), subSequence.end());
+    auto idx = subSeqFound - cars.begin();
+    if (subSeqFound != cars.end()) {
+        std::cout << "Hittade subSequence från index: " << idx << std::endl;
+    }
+    std::cout << "-------------------------------------------------------" << std::endl;
+    std::cout << std::endl;
+    std::cout << "-------------------------------------------------------" << std::endl;
+    std::cout << "6. Kolla om arrayen och vectorn har samma innehåll, det görs med std::equal" << std::endl;
 
     return 0;
 }
